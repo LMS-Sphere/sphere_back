091aa027a14bd171e3bfc4aa3e51800ac01be2a8
assilbelhaj <100833883+assilbelhaj@users.noreply.github.com>
2025-07-30 14:40:56 +0100

    first commit

---
+++ routers/badgeRouter.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const { createBadge, getBadges, getBadge, updateBadge, deleteBadge, upload } = require('../controllers/badgeController');   
+router.post('/', upload.single('image'), createBadge);
+router.get('/', getBadges);
+router.get('/:id', getBadge);
+router.put('/:id', updateBadge);
+router.delete('/:id', deleteBadge);
+
+module.exports = router;
